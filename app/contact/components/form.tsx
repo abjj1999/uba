8d6264aba188c11a2170c377a570eb4745927c90
Abdullah al hilfi <abdullahalhilfi21@gmail.com>
2024-02-19 18:29:25 -0600

    demo2

--- app/contact/components/form.tsx
+++ app/contact/components/form.tsx
@@ -154,7 +154,7 @@ export default function Form() {
             type="submit"
             className="block w-full rounded-md px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-gray-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
           >
-            Let's talk
+            Lets talk
           </Button>
         </div>
       </form>
